1d2064689efe110df003e66b533dd9f624fcea89
MalikAmirJaved <aqabjavedmalik2002@gmail.com>
2026-07-22 18:22:40 +0500

    enhanced

--- web/src/components/providers.tsx
+++ web/src/components/providers.tsx
@@ -2,6 +2,8 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/lib/store/redux";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -12,14 +14,16 @@ export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <AuthProvider>
-          <PermissionsProvider>{children}</PermissionsProvider>
-        </AuthProvider>
-      </TooltipProvider>
-    </QueryClientProvider>
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <AuthProvider>
+            <PermissionsProvider>{children}</PermissionsProvider>
+          </AuthProvider>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </ReduxProvider>
   );
 }
